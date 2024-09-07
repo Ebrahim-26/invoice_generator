4535a8de33cc4e5caf9a3b6f822d410cbc3cacfb
ebrahim <ebrahim@victopialabs.com>
2024-09-07 12:59:10 +0530

    bill page

--- invoice_gen/invoice_generator/src/app/components/BillGen.jsx
+++ invoice_gen/invoice_generator/src/app/components/BillGen.jsx
@@ -1,27 +1,94 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
+import MuiTable from "./MuiTable";
 
 function BillGen() {
-    const data={gst:'12wer32', invoice:'34EEF', date:'12/02/12'}
+  const data = {
+    gst: "12wer32",
+    invoice: "34EEF",
+    date: "26 Feb, 2024",
+    billed_to: "Anna nagar, 2nd street, 4th block, Chennai-67",
+    billed_from: "Nungabakam, 2nd street, 4th block, Chennai-34",
+    acc_num: "876543",
+    bank: "SBI",
+    ifsc: "SBU0004",
+  };
   return (
     <div>
-      <div className='root'>
-        <div className='Big text and logo flex'>
-            <div>
-                <Typography sx={{color:'black', fontSize:'2rem'}}>Purchase Order</Typography>
-            </div>
-            <div className='text-black'>
-               IMAGE LOGO
-            </div>
+      <div className="root">
+        <div className="p-[2rem] grid grid-col-1 gap-4 ">
+          <div className="grid grid-col-1 justify-center text-center bg-slate-400">
+            <Typography
+              sx={{
+                color: "black",
+                fontSize: "2rem",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              INVOICE
+            </Typography>
+            <div className="text-black logo text-lg">Elantrizo</div>
+          </div>
+          <div className="text-black grid grid-cols-1 justify-end text-right">
+            <Typography sx={{ fontWeight: "bold" }}>
+              GSTIN: <span style={{ fontWeight: "normal" }}>{data.gst}</span>{" "}
+            </Typography>
+            <Typography sx={{ fontWeight: "bold" }}>
+              Invoice:{" "}
+              <span style={{ fontWeight: "normal" }}>{data.invoice}</span>
+            </Typography>
+            <Typography sx={{ fontWeight: "bold", color: "black" }}>
+              Date: <span style={{ fontWeight: "normal" }}>{data.date}</span>
+            </Typography>
+          </div>
+          <div className="billing address flex text-black justify-between gap-x-[5rem] ">
+            <Typography>
+              <span className="font-bold"> Billed to:</span> <br />{" "}
+              {data.billed_to}
+            </Typography>
+            <Typography>
+              <span className="font-bold"> From:</span> <br />{" "}
+              {data.billed_from}
+            </Typography>
+          </div>
         </div>
-        <div className='text-black'>
-            <Typography sx={{fontWeight:'bold'}}> GSTIN: <span style={{fontWeight:'normal'}}>{data.gst}</span> </Typography>
-            <Typography sx={{fontWeight:'bold'}}>Invoice: <span style={{fontWeight:'normal'}}>{data.invoice}</span></Typography>
-            <Typography sx={{fontWeight:'bold'}}>Date: <span style={{fontWeight:'normal'}}>{data.date}</span></Typography>
+        <div>
+          <MuiTable />
+        </div>
+        <div className="flex justify-between mt-4">
+          <div className="text-black ">
+            <Typography>Bank Details</Typography>
+            <Typography>
+              <span style={{ fontWeight: "bold" }}>Account Number: </span>
+              {data.acc_num}
+            </Typography>
+            <Typography>
+              <span style={{ fontWeight: "bold" }}>Bank: </span>
+              {data.bank}{" "}
+            </Typography>
+            <Typography>
+              <span style={{ fontWeight: "bold" }}>IFSC: </span> {data.ifsc}
+            </Typography>
+          </div>
+          <div className="flex">
+            <div className="bg-slate-300 font-bold text-right p-4">
+              <Typography>Subtotal: </Typography>
+              <Typography>GST: </Typography>
+              <Typography>Paid: </Typography>
+              <Typography>Yet to be paid: </Typography>
+            </div>
+            <div className="bg-slate-700 text-left p-4">
+              <Typography>1,231 </Typography>
+              <Typography>4,223 </Typography>
+              <Typography>423 </Typography>
+              <Typography>43,223 </Typography>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default BillGen
+export default BillGen;
--- invoice_gen/invoice_generator/src/app/components/DetailsPage.jsx
+++ invoice_gen/invoice_generator/src/app/components/DetailsPage.jsx
@@ -1,28 +1,42 @@
-import { TextField, Typography } from '@mui/material'
-import React from 'react'
+import { TextField, Typography } from "@mui/material";
+import React from "react";
 
 function DetailsPage() {
-    const name={name:'Shahil'}
+  const name = { name: "Ebrahim" };
   return (
-    <div className='flex justify-center items-center h-screen  shadow-2xl'>
+    <div className="flex justify-center items-center h-screen  shadow-2xl">
+      <div>
         <div>
-        <div>
-            <div className=' mb-2 flex justify-center border-4 rounded-md gap-2 border-black bg-[#8b9cef]'>
-            <Typography sx={{color:'black', fontSize:"2rem"}}>{name.name}</Typography>
-            </div>
-            <div className='grid grid-cols-1 w-[25rem] border-4 rounded-md border-black p-10 gap-y-3 bg-[#e1debe]'  >
-                <TextField color='secondary' placeholder='Company Name'size='small'/>
-                <TextField color='secondary' placeholder='Email' size='small'/>
-                <TextField color='secondary' placeholder='Phone Number'size='small'/>
-                <TextField color='secondary' placeholder='Address' size='small'/>
-                <TextField color='secondary' placeholder='Contact Person Name' size='small'/>
-                <TextField color='secondary' placeholder='Notes' size='small'/>
-                <TextField color='secondary' placeholder='GSTIN' size='small'/>
-            </div>
-            </div>
+          <div className=" mb-2 flex justify-center border-4 rounded-md gap-2 border-black bg-[#8b9cef]">
+            <Typography sx={{ color: "black", fontSize: "2rem" }}>
+              {name.name}
+            </Typography>
+          </div>
+          <div className="grid grid-cols-1 w-[25rem] border-4 rounded-md border-black p-10 gap-y-3 bg-[#e1debe]">
+            <TextField
+              color="secondary"
+              placeholder="Company Name"
+              size="small"
+            />
+            <TextField color="secondary" placeholder="Email" size="small" />
+            <TextField
+              color="secondary"
+              placeholder="Phone Number"
+              size="small"
+            />
+            <TextField color="secondary" placeholder="Address" size="small" />
+            <TextField
+              color="secondary"
+              placeholder="Contact Person Name"
+              size="small"
+            />
+            <TextField color="secondary" placeholder="Notes" size="small" />
+            <TextField color="secondary" placeholder="GSTIN" size="small" />
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default DetailsPage
+export default DetailsPage;
